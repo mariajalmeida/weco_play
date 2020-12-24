649078f95095ac03f82b952ea841ef26c2539186
mariajalmeida <70196707+mariajalmeida@users.noreply.github.com>
2020-12-24 18:58:07 +0100

    Add files via upload

---
+++ script/games.js
@@ -0,0 +1,75 @@
+let games = "https://mariajalmeida.com/KEA/2nd_semester/weco_play/wp-json/wp/v2/page_detail?_embed&per_page=20&_fields=id,title,_links";
+
+window.addEventListener("DOMContentLoaded", getGames);
+
+// fetch all of our data
+function getGames() {
+    fetch(games)
+        .then(res => res.json())
+        .then(processData);
+}
+
+// start to divide the data
+function processData(data) {
+    // console.log("here are the games");
+    //  console.log(data);
+    data.reverse();
+    data.forEach(retrieveSingleGame);
+}
+
+// call in each single game and do magical things
+function retrieveSingleGame(gameDivision) {
+    // console.log(gameDivision);
+    // console.log("hey game");
+
+    const template = document.querySelector("#games").content;
+    const clone = template.cloneNode(true);
+
+
+    //add categories to games for filtering filters 
+    clone.querySelector('.box_container').classList.add(gameDivision._embedded["wp:term"][0][0].slug);
+
+    const title = clone.querySelector("h2");
+    title.textContent = gameDivision.title.rendered;
+    const images = gameDivision._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    clone.querySelector("img").src = images;
+
+    const link = clone.querySelector("a");
+    link.href += gameDivision.id;
+
+    // append child
+    document.querySelector("main").appendChild(clone);
+}
+
+
+//EVENT FOR FILTERS
+const categories = document.querySelectorAll(".categories button");
+categories.forEach(button => button.addEventListener('click', filterData));
+//Class active
+function filterData(e) {
+    const clicked = e.target.id;
+    categories.forEach((btn) => {
+        if (btn.id == clicked) {
+            btn.classList.add("active");
+        } else {
+            btn.classList.remove("active");
+        }
+    })
+    //Filter games
+    const allBoxes = document.querySelectorAll('.box_container');
+    allBoxes.forEach((box) => {
+        if (box.classList[1] == clicked) {
+            box.classList.remove("hide");
+        } else {
+            box.classList.add("hide")
+        }
+    //     //showAll
+        const btnAll = document.querySelector("#all");
+        btnAll.addEventListener("click", showAll);
+
+        function showAll() {
+            box.classList.remove("hide")
+        }
+    });
+}
+
---
+++ script/single-game.js
@@ -0,0 +1,134 @@
+window.addEventListener("DOMContentLoaded", getData);
+
+const gamesInfo = "https://mariajalmeida.com/KEA/2nd_semester/weco_play/wp-json/wp/v2/page_detail?per_page=2&filter[orderby]=rand&_embed";
+
+function getData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log("URLSearchParams " + window.location);
+    const the_game_id = urlParams.get("page");
+    console.log(the_game_id);
+
+
+    //    fetch("https://mariajalmeida.com/KEA/2nd_semester/weco_play/wp-json/wp/v2/page_detail/25?_embed")
+    fetch("https://mariajalmeida.com/KEA/2nd_semester/weco_play/wp-json/wp/v2/page_detail/" + the_game_id + "?_embed")
+        .then(res => res.json())
+        .then(stuffReceived);
+
+    setTimeout(function () {
+        fetch(gamesInfo)
+            .then((res) => {
+                return res.json();
+            })
+            .then(cardsRetrieved);
+    }, 200)
+
+}
+
+
+function stuffReceived(stuff) {
+    showGame(stuff);
+    createImages(stuff);
+    createFeatures(stuff);
+    createIngame(stuff);
+}
+
+// here's the flags
+function createImages(lan) {
+    lan.languages.forEach((lang) => {
+        const lan_img = document.createElement("img");
+        lan_img.src = lang.guid;
+        document.querySelector(".languages").appendChild(lan_img);
+    })
+}
+
+// here's the in-game images
+function createIngame(ingame) {
+    ingame.ingame_screenshots.forEach((cap) => {
+        const cap_image = document.createElement("img");
+        cap_image.src = cap.guid;
+        document.querySelector(".game_caps").appendChild(cap_image);
+    })
+}
+
+// here's weco's guaranteed features
+function createFeatures(ft) {
+    ft.features.forEach(feature => {
+        const weco_feature = document.createElement("p");
+        weco_feature.textContent = feature.post_title;
+        document.querySelector(".inner_features").appendChild(weco_feature);
+    })
+}
+
+// here's most of the fetched page
+function showGame(game) {
+    console.log(game);
+
+    const template = document.querySelector("#single_game").content;
+    const clone = template.cloneNode(true);
+
+    // populate with information
+    const header = game.header.guid;
+    clone.querySelector(".overlay img").src = header;
+    const images = game._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+    clone.querySelector(".header_container img").src = images;
+    const h1 = clone.querySelector(".header_description h1");
+    h1.textContent = game.title.rendered;
+    const shortDescription = clone.querySelector(".header_description p");
+    shortDescription.innerHTML = game.excerpt.rendered;
+
+    const longDescription = clone.querySelector(".long_description p");
+    longDescription.innerHTML = game.content.rendered;
+    const fileSize = clone.querySelector(".file_size");
+    fileSize.textContent = game.file_size;
+    const releaseDate = clone.querySelector(".release_date");
+    releaseDate.textContent = game.release_date;
+    const players = clone.querySelector(".players");
+    players.textContent = game.players;
+    const category = clone.querySelector(".category");
+    category.textContent = game.genre;
+
+    // append child
+    document.querySelector("main").appendChild(clone);
+}
+
+// fetched cards here
+function cardsRetrieved(stuff) {
+    similarCards(stuff);
+    createCards(stuff);
+}
+
+function createCards(card) {
+    console.log(card)
+    console.log("here's my card")
+    card.forEach(c => {
+        const sim_img = document.createElement("img");
+        console.log(c, "hey");
+        sim_img.src = c._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+
+        const subtitle = document.createElement("h3");
+        subtitle.textContent = c.title.rendered;
+
+        const section = document.createElement("section");
+
+        const link = document.createElement("a");
+        link.href = "single-game.html?page=";
+
+        const idlink = link;
+        link.href += c.id;
+
+        section.appendChild(sim_img);
+        section.appendChild(subtitle);
+        link.appendChild(section);
+        document.querySelector(".inner_similar_games").appendChild(link);
+    })
+}
+
+function similarCards(g) {
+    const template = document.querySelector("#similar_games").content;
+    const clone = template.cloneNode(true);
+
+    // content
+
+    // append child
+    document.querySelector("main").appendChild(clone);
+}
